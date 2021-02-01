661232f8d8b7845cd6b890cd307b1d80b813192b
Bertrand Thomas <bertrand@devpro.fr>
2021-02-01 09:18:30 +0100

    Hide echo commands in foreground

--- dojo-git-basics/foreground.sh
+++ dojo-git-basics/foreground.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Waiting for background process (git update) to complete"
+{
+  echo "Waiting for background process (git update) to complete"
 
-while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
+  while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
 
-rm /opt/.backgroundfinished
+  rm /opt/.backgroundfinished
 
-echo "Done"
+  echo "Done"
+} &> /dev/null
